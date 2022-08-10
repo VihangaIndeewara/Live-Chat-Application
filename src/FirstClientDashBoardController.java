5a7e15298eef5f72e1b32efa126f83c3812ec891
VihangaIndeewra <indeewravihnga@gmail.com>
2022-08-10 18:30:33 +0530

    Created First Client DashBoard

---
+++ src/FirstClientDashBoardController.java
@@ -0,0 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FirstClientDashBoardController {
+    public TextArea txtArea;
+    public TextField txtField;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/FirstClientFormController.java
+++ src/FirstClientFormController.java
@@ -1,2 +1,16 @@
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class FirstClientFormController {
+
+    public JFXTextField txtUserName;
+
+    public void btnLogInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void setUi(String location){
+
+    }
 }
